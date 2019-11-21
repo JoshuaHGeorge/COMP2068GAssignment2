0a2b6d503d1a47c0be8ae1a784102f33a9220d8a
JoshuaHGeorge <jwatime@gmail.com>
2019-11-21 16:45:27 -0500

    finished login and crud functionality

--- app.js
+++ app.js
@@ -27,6 +27,62 @@ mongoose.connect(globals.db, {
   console.log('Abort mission')
 })
 
+// code from class for configuring passport
+// make the passport
+app.use(session({
+    secret:'mycr@zy$ecret$tring'
+}))
+
+// passport setup
+app.use(passport.initialize())
+app.use(passport.session())
+
+// 3. link passport to our User model
+const User = require('./models/user')
+passport.use(User.createStrategy())
+
+// 4. set up passport to read / write user data to the session object
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
+
+// googles proprietary auth stuff
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+passport.use(new GoogleStrategy({
+        clientID: globals.ids.google.clientID,
+        clientSecret: globals.ids.google.clientSecret,
+        callbackURL: globals.ids.google.callbackURL
+    },
+    (token, tokenSecret, profile, done) => {
+        // do we already have a User document in MongoDB for this Google profile?
+        User.findOne({oauthId: profile.id}, (err, user) => {
+            if (err) {
+                console.log(err) // error, so stop and debug
+            }
+            if (!err && user != null) {
+                // Google already exists in our MongoDB so just return the user object
+                done(null, user)
+            }
+            else {
+                // Google user is new, register them in MongoDB users collection
+                user = new User({
+                    oauthId: profile.id,
+                    username: profile.displayName,
+                    oauthProvider: 'Google',
+                    created: Date.now()
+                })
+
+                user.save((err) => {
+                    if (err) {
+                        console.log(err)
+                    }
+                    else {
+                        done(null, user)
+                    }
+                })
+            }
+        })
+    }
+))
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -40,8 +96,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
-//app.use('/assignments', assignmentsRouter);
+// what should be the connection to my assignments controller that for some reason crashes the program
+app.use('/assignments', assignmentsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- controllers/assignments.js
+++ controllers/assignments.js
@@ -3,13 +3,16 @@ const express = require('express');
 const router = express.Router();
 
 // get the data model
-const Assignments = require('../models/assignment.js');
+const Assignment = require('../models/assignment.js');
+
+// use the npm for logging in
+const passport = require('passport')
 
 // get function for index page
-router.get('/', (req, res, next) => {
+router.get('/', loggedIn, (req, res, next) => {
     console.log("Ran the function")
     // get the info from the database
-    Assignments.find((err, assign) => {
+    Assignment.find((err, assignments) => {
         if(err) {
             console.log(err)
             res.end(err)
@@ -17,20 +20,37 @@ router.get('/', (req, res, next) => {
         else{
             res.render('assignments/index', {
                 title: 'Assignments', // set title
-                upcoming: assign // send the list of info from the database
+                user: req.user,
+                upcoming: assignments // send the list of info from the database
             });
         }
     })
 });
 
-router.get('/add', (req, res) => {
+// check whether the user is logged in
+function loggedIn(req,res,next){
+    // send the user to the login page if they are not logged in
+    if (req.isAuthenticated()){
+        return next()
+    }
+    else{
+        res.redirect('/login')
+    }
+}
+
+// load the page for adding new assignments
+router.get('/add', loggedIn, (req, res) => {
     res.render('assignments/add')
+})
+// load the page for editing old assignments
+router.get('/edit', loggedIn, (req, res) => {
+    res.render('assignments/edit')
 })
 
 /* POST orders/add form submission */
-router.post('/add', (req, res) => {
-    // save a new order on mongoose or catch trying
-    Order.create({
+router.post('/add', loggedIn, (req, res) => {
+    // add a new assignment to the database
+    Assignment.create({
         course: req.body.course,
         work: req.body.work,
         link: req.body.link,
@@ -41,7 +61,50 @@ router.post('/add', (req, res) => {
             res.end(err)
         }
         else {
-            res.redirect('/orders')
+            res.redirect('/assignments')
         }
     })
 })
+
+router.get('/delete/:_id', loggedIn, (req, res) => {
+    // take out the assignment as specified by its id
+    Assignment.remove({ _id: req.params._id }, (err) => {
+        if (err) {
+            console.log(err)
+            res.end(err)
+        }
+        else {
+            res.redirect('/assignments')
+        }
+    })
+})
+
+
+// what happens when you hit the submit button on the edit page
+router.post('/edit/:_id', loggedIn, (req, res) => {
+    // get the id for the task
+    var _id = req.params._id
+
+    // make a new assignment to replace the old one with
+   var holder = new Assignment({
+        _id: _id,
+        name: req.body.course,
+        priority: req.body.work,
+        due: req.body.due,
+        link: req.body.link
+    })
+
+    // put the new assignment where the old one was
+    Assignment.updateOne({ _id: _id }, holder, (err) => {
+        if (err) {
+            console.log(err)
+            res.end(err)
+        }
+        else {
+            res.redirect('/assignments')
+        }
+    })
+})
+
+// set the controller to be publically accessible
+module.exports = router
