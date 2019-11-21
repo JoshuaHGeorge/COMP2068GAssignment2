0ebc0d0f8e6a2f59f6cd6a94f4c90b16d9f55331
JoshuaHGeorge <jwatime@gmail.com>
2019-11-21 11:19:36 -0500

    added mongo db connection string

---
+++ config/globals.js
@@ -0,0 +1,4 @@
+// make a very large amount of ado in order to store the database connection in a globally accessible place
+module.exports = {
+    'db': 'mongodb+srv://Josh:VUYS2UAerZxr4kVA@cluster0-b4pbi.mongodb.net/test?retryWrites=true&w=majority'
+}
